e89dbd9329f9a89620d9645d7c17bd33e7ba0b50
徐腾 <45660998+BNDS-tx@users.noreply.github.com>
2026-01-11 01:27:17 +0000

    撤回不完善的出入胶液改动，改回默原版的更改但保留部分来自上游的修复

--- src/main/java/net/ltxprogrammer/changed/block/WhiteLatexTransportInterface.java
+++ src/main/java/net/ltxprogrammer/changed/block/WhiteLatexTransportInterface.java
@@ -22,8 +22,6 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.Optional;
-
 public interface WhiteLatexTransportInterface {
     static boolean isEntityInWhiteLatex(LivingEntity entity) {
         if (entity instanceof PlayerDataExtension ext)
@@ -59,7 +57,7 @@ public interface WhiteLatexTransportInterface {
 
         entity.playSound(ChangedSounds.ENTITY_ENTER_LATEX.get(), 1.0f, 1.0f);
         if (!UniversalDist.isClientRemotePlayer(entity)) {
-            final Vec3 center = new Vec3(0.5D, 0.5D, 0.5D);
+            final Vec3 center = entity.position().subtract(Vec3.atLowerCornerOf(pos));
             Vec3 surface = LatexCoverState.getAt(entity.level(), pos).findClosestSurface(center, null);
             Vec3 delta = surface.subtract(center);
             final Direction closestDirection = center.equals(surface) ? null : Direction.getNearest(delta.x, delta.y, delta.z);
@@ -75,15 +73,24 @@ public interface WhiteLatexTransportInterface {
         }
     }
 
-    static Optional<BlockPos> isBoundingBoxInWhiteLatex(LivingEntity entity) {
+    static boolean isBoundingBoxInWhiteLatex(LivingEntity entity) {
         AABB testHitbox = entity.getBoundingBox().inflate(-0.05);
-        return BlockPos.betweenClosedStream(testHitbox).filter(blockPos -> {
+        return BlockPos.betweenClosedStream(testHitbox).anyMatch(blockPos -> {
             final BlockState blockState = entity.level().getBlockState(blockPos);
             if (blockState.getBlock() instanceof WhiteLatexTransportInterface transportInterface)
                 return transportInterface.allowTransport(blockState);
 
             return false;
-        }).findFirst();
+        });
+    }
+
+    static boolean isStandingOnLatex(LivingEntity entity) {
+        BlockPos blockPos = BlockPos.containing(entity.getPosition(1.0F));
+
+        BlockState blockAtFoot = entity.getCommandSenderWorld().getBlockState(blockPos);
+
+        return blockAtFoot.getBlock() == ChangedBlocks.WHITE_LATEX_FLUID.get()
+                || blockAtFoot.getBlock() == ChangedBlocks.WHITE_LATEX_BLOCK.get();
     }
 
     @Mod.EventBusSubscriber
@@ -96,17 +103,19 @@ public interface WhiteLatexTransportInterface {
             if (isEntityInWhiteLatex(event.player))
                 return;
 
-            isBoundingBoxInWhiteLatex(event.player).ifPresent(latexPosition -> {
+            if (isBoundingBoxInWhiteLatex(event.player) &&
+                    (LatexCoverState.getAt(event.player.level(), new BlockPos(event.player.getBlockX(), event.player.getBlockY(), event.player.getBlockZ()))
+                            .getType() == ChangedLatexTypes.WHITE_LATEX.get()) || isStandingOnLatex(event.player)) {
                 ProcessTransfur.ifPlayerTransfurred(event.player, variant -> {
                     if (variant.getLatexType() == ChangedLatexTypes.WHITE_LATEX.get())
-                        entityEnterLatex(event.player, latexPosition);
+                        entityEnterLatex(event.player, new BlockPos(event.player.getBlockX(), event.player.getBlockY(), event.player.getBlockZ()));
                     else if (ChangedLatexTypes.WHITE_LATEX.get().isHostileTo(variant.getLatexType()))
                         event.player.hurt(ChangedDamageSources.WHITE_LATEX.source(event.player.level().registryAccess()), 2.0f);
                 }, () -> {
                     if (ProcessTransfur.progressTransfur(event.player, 4.8f, ChangedTransfurVariants.PURE_WHITE_LATEX_WOLF.get(), TransfurContext.hazard(TransfurCause.WHITE_LATEX)))
-                        entityEnterLatex(event.player, latexPosition);
+                        entityEnterLatex(event.player, new BlockPos(event.player.getBlockX(), event.player.getBlockY(), event.player.getBlockZ()));
                 });
-            });
+            }
         }
     }
 }
