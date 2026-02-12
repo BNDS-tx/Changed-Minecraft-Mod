36ff84c1d7e5c40f524e584fd3c5601e4a98b031
徐腾 <45660998+BNDS-tx@users.noreply.github.com>
2026-02-12 03:43:10 +0000

    修复了外骨骼在某些情况下客服端不同步导致无限麻痹的问题。

--- src/main/java/net/ltxprogrammer/changed/item/ExoskeletonItem.java
+++ src/main/java/net/ltxprogrammer/changed/item/ExoskeletonItem.java
@@ -248,7 +248,8 @@ public class ExoskeletonItem<T extends AbstractRobot> extends PlaceableEntity<T>
                 } else degradeCharge(slotContext, 1);
         }
 
-        if (slotContext.wearer().isInWaterOrRain() && !ignoreDamage) {
+        if (!slotContext.wearer().level.isClientSide
+                && slotContext.wearer().isInWaterOrRain() && !ignoreDamage) {
             int rate = slotContext.wearer().isInWater() ? 20 : 40;
 
             if (slotContext.wearer().tickCount % rate == 0) {
